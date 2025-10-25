97d9d8e0594a0f650a5a809feb4115f44842c4ad
sreeja200 <124538622+sreeja200@users.noreply.github.com>
2025-10-25 18:18:03 +0530

    Update API endpoint for popular products

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
   const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/popularinwomen")
+    fetch("https://e-commerce-mern-backend-5rue.onrender.com/popularinwomen")
     .then((response) => response.json())
     .then((data) => setPopularProducts(data));
   }, []);
