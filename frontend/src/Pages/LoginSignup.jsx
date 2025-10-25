892a0e0f825392cdea1cde1672baa45476f68125
sreeja200 <124538622+sreeja200@users.noreply.github.com>
2025-10-25 18:54:09 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -18,7 +18,7 @@ const LoginSignup = () => {
     console.log("Login Function Executed", formData);
     console.log("Signup Function Executed", formData);
   let responseData;
-  await fetch('http://localhost:4000/login', {
+  await fetch('https://e-commerce-mern-backend-5rue.onrender.com/login', {
     method: 'POST',
     headers: {
       Accept: 'application/form-data',
@@ -43,7 +43,7 @@ const LoginSignup = () => {
   const signup = async () => {
   console.log("Signup Function Executed", formData);
   let responseData;
-  await fetch('http://localhost:4000/signup', {
+  await fetch('https://e-commerce-mern-backend-5rue.onrender.com/signup', {
     method: 'POST',
     headers: {
       Accept: 'application/form-data',
