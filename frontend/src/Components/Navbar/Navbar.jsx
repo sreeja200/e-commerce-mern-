97da92ff98ad2f7e82756f8c51adf769826a9c1c
sreeja200 <124538622+sreeja200@users.noreply.github.com>
2025-10-28 09:30:52 +0530

    Style the Login button in Navbar component

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
         <div className='nav-login-cart'>
           {localStorage.getItem('auth-token')
            ? <button className="nav-login-btn" onClick={()=>{localStorage.removeItem('auth-token'); window.location.replace('/')}}>Logout</button>
-           : <Link to='/login'><button>Login</button></Link>
+           : <Link to='/login'><button className="nav-login-btn">Login</button></Link>
           }
           <Link to="/cart"><img src={cart_icon} alt="" /></Link>
           <div className='nav-cart-count'>{getTotalCartItems()}</div>
